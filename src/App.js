7dffec547efc9c630790d101ea1d31d587a41e8d
Ilfat <ilfat_gatin@mail.ru>
2022-05-19 15:45:17 +0300

    homework 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,57 @@
 import Message from "./Message";
 import './index.css'
-
-const message = "Hi React!"
+import { useEffect, useState } from "react";
 
 function App() {
+    
+  const [messageList, setMessageList] = useState([])  
+
+  const [author, setAuthor] = useState('')
+
+  const [text, setText] = useState('')
+
+  useEffect(()=>{
+    if (messageList.length > 0) {
+      if (messageList[messageList.length-1].author !== "Robot") {
+        const timeout = setTimeout(() => {
+          setMessageList((messageList) =>[...messageList, {author: "Robot", text: `Welcome, ${author}!`}])
+        }, 1500)
+      }     
+    }
+  }, [messageList, author])
+
+  const addNewMessage = () => {
+    const key = Date.now()
+    setMessageList((messageList) => [...messageList, {author: author, text: text, key: key}] 
+    )
+  }  
+
   return (
     <div className="wrapper">
-      <Message text={message}/>
+      <h1>Chat</h1>
+      <div>
+        <div className="messageList">
+        {messageList.map( (message) => {
+        return <Message author={message.author} text={message.text} />
+        } )}
+        </div>        
+          <input 
+            type="text" 
+            className="input" 
+            placeholder="Your name"
+            value={author}
+            onChange={(event) => {setAuthor(event.target.value)}}></input>
+          <input 
+            type="text" 
+            className="input" 
+            placeholder="Your message"
+            value={text}
+            onChange={(event) => {setText(event.target.value)}}></input>   
+          <button 
+            className="button" 
+            onClick={addNewMessage}
+            >Send</button>
+      </div>     
     </div>
   );
 }
